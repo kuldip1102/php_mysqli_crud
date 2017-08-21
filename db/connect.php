19553f6c81ee61667e86f215068fa3f4ee3f2648
koolchantu <kuldip.suthar1102@gmail.com>
2017-08-21 15:04:30 +0530

    db/connect.php : database connection "app" of the mysqli database.

---
+++ db/connect.php
@@ -0,0 +1,17 @@
+<?php 
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "app";
+
+$conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+// * show the error no	
+	// echo $conn->connect_errno;
+
+if ($conn->connect_errno) {
+	echo $conn->connect_error;
+	// die("There were some error");
+}
+
+?>
