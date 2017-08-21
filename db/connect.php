642532768bd2e4197a067afe183220c234de8df0
koolchantu <kuldip.suthar1102@gmail.com>
2017-08-21 21:41:42 +0530

    db/connect.php : just declare connection object global.

--- db/connect.php
+++ db/connect.php
@@ -4,7 +4,7 @@ $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "app";
-
+global $conn;
 $conn = new mysqli($dbhost,$dbuser,$dbpass,$dbname);
 // * show the error no	
 	// echo $conn->connect_errno;
